2a010fefe118abc4cf8b4fc384c51b00246db3c4
Vladimir Tolmachev <tolm_vl@hotmail.com>
2019-10-21 08:14:20 +0700

    temp

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
 import urllib2
 import time
 
-print 'Run client...'
+print('Run client...')
 
 url = 'http://192.168.0.105:8045'
 url = '{}/result?code=1&message=test_1'.format(url)
-print 'Response: ' + urllib2.urlopen(url).read()
+print('Response: ' + urllib2.urlopen(url).read())
 
 
 time.sleep(5)
-print 'exit.'
+print('exit.')
 exit(1)
